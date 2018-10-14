b2051e454029b631668376668f0cb60758b9703b
Luis José Sánchez <luisjose@espiral.io>
2018-10-14 20:03:06 +0200

    Updated home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'Ejercicio01', loadChildren: './ejercicio01/ejercicio01.module#Ejercicio01PageModule' },
-  { path: 'Ejercicio02', loadChildren: './ejercicio02/ejercicio02.module#Ejercicio02PageModule' },
-  { path: 'Ejercicio03', loadChildren: './ejercicio03/ejercicio03.module#Ejercicio03PageModule' },
-  { path: 'Ejercicio04', loadChildren: './ejercicio04/ejercicio04.module#Ejercicio04PageModule' },
-  { path: 'Ejercicio05', loadChildren: './ejercicio05/ejercicio05.module#Ejercicio05PageModule' },
-  { path: 'Ejercicio06', loadChildren: './ejercicio06/ejercicio06.module#Ejercicio06PageModule' },
-  { path: 'Ejercicio07', loadChildren: './ejercicio07/ejercicio07.module#Ejercicio07PageModule' },
-  { path: 'Ejercicio08', loadChildren: './ejercicio08/ejercicio08.module#Ejercicio08PageModule' },
-  { path: 'Ejercicio09', loadChildren: './ejercicio09/ejercicio09.module#Ejercicio09PageModule' },
-  { path: 'Ejercicio10', loadChildren: './ejercicio10/ejercicio10.module#Ejercicio10PageModule' },
+  { path: 'ejercicio01', loadChildren: './ejercicio01/ejercicio01.module#Ejercicio01PageModule' },
+  { path: 'ejercicio02', loadChildren: './ejercicio02/ejercicio02.module#Ejercicio02PageModule' },
+  { path: 'ejercicio03', loadChildren: './ejercicio03/ejercicio03.module#Ejercicio03PageModule' },
+  { path: 'ejercicio04', loadChildren: './ejercicio04/ejercicio04.module#Ejercicio04PageModule' },
+  { path: 'ejercicio05', loadChildren: './ejercicio05/ejercicio05.module#Ejercicio05PageModule' },
+  { path: 'ejercicio06', loadChildren: './ejercicio06/ejercicio06.module#Ejercicio06PageModule' },
+  { path: 'ejercicio07', loadChildren: './ejercicio07/ejercicio07.module#Ejercicio07PageModule' },
+  { path: 'ejercicio08', loadChildren: './ejercicio08/ejercicio08.module#Ejercicio08PageModule' },
+  { path: 'ejercicio09', loadChildren: './ejercicio09/ejercicio09.module#Ejercicio09PageModule' },
+  { path: 'ejercicio10', loadChildren: './ejercicio10/ejercicio10.module#Ejercicio10PageModule' },
 ];
 
 @NgModule({
